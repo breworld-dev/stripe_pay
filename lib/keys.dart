39cf5f4ccad159638ae6c08f5a2e28bd67bbaf38
Brendon <80946312+breworld-dev@users.noreply.github.com>
2024-12-21 17:50:19 +0300

    Update keys.dart
    Test API Secret Key removed

--- lib/keys.dart
+++ lib/keys.dart
@@ -2,4 +2,4 @@
 const String stripePublishableKey =
     "pk_test_51PeirlRsNGAAvnZXWLHnyj2DXeMZvhEDF3WFP7DRsTwL9G6vDYYVklVLFeX9tZMhppEpWCs3rgl3E85cm2q2z3gc003X0XYN5H";
 const String stripeSecretKey =
-    "sk_test_51PeirlRsNGAAvnZXv9k9ZtVQSbskxvns1A0bvUhcbGI188MFUtcV3XzFbeq4J6PGSdbhthrglsJIhwML6rFfdAwB00fSRh4rWG";
+    "";
